341fb6a36464a91a728df4b5e06aa25c0b1a3d0e
rccdoes <itschauhan101@gmail.com>
2025-06-16 20:25:10 +0530

    js

--- script.js
+++ script.js
@@ -2,21 +2,31 @@
 
 const apikey = "737ab23afa4b565ff2c4727ff1ca6deb"
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
+const searchbox = document.querySelector(".search input");
+
+const searchbtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
 
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerText = data.name;
-    document.querySelector(".temp").innerText = data.main.temp + "°C";
+    document.querySelector(".temp").innerText = Math.round(data.main.temp) + "°C";
 
     document.querySelector(".humidity").innerText = data.main.humidity  + "%";
     document.querySelector(".wind").innerText = data.wind.speed + "km/h";
 
 
 }
+
+searchbtn.addEventListener("click", () => {
+    checkWeather(searchbox.value);
+});
+
+
 checkWeather();
