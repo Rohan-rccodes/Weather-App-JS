69658eac0b27452310edf4c526906deec40e8ae0
rccdoes <itschauhan101@gmail.com>
2025-06-18 20:03:38 +0530

    new weather app

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const searchbox = document.querySelector(".search input");
 
 const searchbtn = document.querySelector(".search button");
 
+const weathericon = document.querySelector(".weather-icon");
+
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apikey}`);
 
@@ -16,11 +18,33 @@ async function checkWeather(city) {
     console.log(data);
 
     document.querySelector(".city").innerText = data.name;
+
     document.querySelector(".temp").innerText = Math.round(data.main.temp) + "°C";
 
     document.querySelector(".humidity").innerText = data.main.humidity  + "%";
+
     document.querySelector(".wind").innerText = data.wind.speed + "km/h";
 
+    if(data.weather[0].main == "Clouds") {
+        weathericon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear") {
+        weathericon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain") {
+        weathericon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weathericon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist") {
+        weathericon.src = "images/mist.png";
+    }
+    else if(data.weather[0].main == "Snow") {
+        weathericon.src = "images/snow.png";
+    }
+    else {
+        weathericon.src = "";
 
 }
 
@@ -29,4 +53,5 @@ searchbtn.addEventListener("click", () => {
 });
 
 
-checkWeather();
+checkWeather()
+}
